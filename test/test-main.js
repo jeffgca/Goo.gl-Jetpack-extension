9ac888436fc8bc55444b9d3c94ac060ad88f5a18
Jeff Griffiths <jeff@canuckistani.ca>
2011-06-20 21:08:11 -0700

    re-factored test to match the removal of shortening.

--- test/test-main.js
+++ test/test-main.js
@@ -1,5 +1,16 @@
 const main = require("main");
+var testRunner, remaininTests;
 
 exports.test_test_run = function(test) {
   test.pass("Unit test running!");
 };
+
+exports.test_shorten = function(test) {
+    testRunner = test;
+    testRunner.waitUntilDone(2000);
+    main.shorten('https://www.mozilla.org/', function(response) {
+        testRunner.assertEqual(/^http:\/\/goo.gl\/[\w]{5}/.test(response.id), true);
+        testRunner.done();
+    });
+}
+
